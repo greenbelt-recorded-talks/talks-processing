67e6a80f96fa9fe30579cc1a5e54ddc53d10bdec
Rob Redpath <rob.redpath@opendataservices.coop>
2023-08-05 12:51:01 +0100

    more black

--- gbtalks/commands.py
+++ gbtalks/commands.py
@@ -21,73 +21,90 @@ def run_command(cmd):
     with semaphore:
         os.system(cmd)
 
+
 def get_path_for_file(talk_id, file_type):
-    return (app.config["TALKS_DIRS"][file_type]["directory"] + 
-        "/gb" + 
-        app.config['GB_FRIDAY'][2:4] + 
-        "-"  + 
-        str(talk_id).zfill(3) + 
-        app.config["TALKS_DIRS"][file_type]["suffix"] + 
-        '.mp3')
+    return (
+        app.config["TALKS_DIRS"][file_type]["directory"]
+        + "/gb"
+        + app.config["GB_FRIDAY"][2:4]
+        + "-"
+        + str(talk_id).zfill(3)
+        + app.config["TALKS_DIRS"][file_type]["suffix"]
+        + ".mp3"
+    )
+
 
 def get_cd_dir_for_talk(talk):
-    return app.config['CD_DIR'] + '/gb' + app.config['GB_FRIDAY'][2:4] + "-"  + str(talk).zfill(3) + '/'
+    return (
+        app.config["CD_DIR"]
+        + "/gb"
+        + app.config["GB_FRIDAY"][2:4]
+        + "-"
+        + str(talk).zfill(3)
+        + "/"
+    )
+
 
 def process_talk(talk):
-    top = AudioSegment.from_file(app.config['TOP_TAIL_DIR'] + '/' + 'top.mp3')
-    tail = AudioSegment.from_file(app.config['TOP_TAIL_DIR'] + '/' + 'tail.mp3')
+    top = AudioSegment.from_file(app.config["TOP_TAIL_DIR"] + "/" + "top.mp3")
+    tail = AudioSegment.from_file(app.config["TOP_TAIL_DIR"] + "/" + "tail.mp3")
 
     # Add the top and tail, create a high-quality mp3
     hq_mp3 = top + AudioSegment.from_file(get_path_for_file(talk.id, "edited")) + tail
 
     # Export a WAV file of the top/tailed audio into /tmp for further processing
-    hq_mp3.export('/tmp/toptailed' + str(talk.id) + '.wav',
-                    format='wav')
+    hq_mp3.export("/tmp/toptailed" + str(talk.id) + ".wav", format="wav")
 
     # Normalise to a fixed level
-    subprocess.call(['ffmpeg-normalize', '/tmp/toptailed' + str(talk.id) + '.wav',
-                    '-o', '/tmp/normalized' + str(talk.id) + '.wav',
-                    '--loudness-range-target', '3',
-                    '-t', '-13', '-f', '-ar', '44100'])
+    subprocess.call(
+        [
+            "ffmpeg-normalize",
+            "/tmp/toptailed" + str(talk.id) + ".wav",
+            "-o",
+            "/tmp/normalized" + str(talk.id) + ".wav",
+            "--loudness-range-target",
+            "3",
+            "-t",
+            "-13",
+            "-f",
+            "-ar",
+            "44100",
+        ]
+    )
 
     # Load the normalised file back in
-    hq_mp3 = AudioSegment.from_file('/tmp/normalized' + str(talk.id) + '.wav')
+    hq_mp3 = AudioSegment.from_file("/tmp/normalized" + str(talk.id) + ".wav")
 
     # Create a reduced-bitrate MP3 from the normalized file
-    hq_mp3.export(get_path_for_file(talk.id, "processed"),
-                    format='mp3',
-                    bitrate='128k')
+    hq_mp3.export(get_path_for_file(talk.id, "processed"), format="mp3", bitrate="128k")
 
     # Put appropriate metadata on the resultant mp3
     mp3 = ID3(get_path_for_file(talk.id, "processed"))
 
-    mp3['TALB'] = TALB(text = "Greenbelt Festival Talks " + app.config['GB_FRIDAY'][:-4])
-    mp3['TCOP'] = TCOP(text = app.config['GB_FRIDAY'][:-4] + " Greenbelt Festivals")
-    mp3['TIT2'] = TIT2(text = talk.title)
-    mp3['TPE1'] = TPE1(text = talk.speaker)
-    mp3['TPE2'] = TPE2(text = talk.speaker)
-    mp3['TRCK'] = TRCK(text = str(talk.id))
-    mp3['TDRC'] = TDRC(text = str(app.config['GB_FRIDAY'][:-4]))
-    mp3['COMM'] = COMM(text = talk.description)
-    mp3['TCMP'] = TCMP(text = '1')
+    mp3["TALB"] = TALB(text="Greenbelt Festival Talks " + app.config["GB_FRIDAY"][:-4])
+    mp3["TCOP"] = TCOP(text=app.config["GB_FRIDAY"][:-4] + " Greenbelt Festivals")
+    mp3["TIT2"] = TIT2(text=talk.title)
+    mp3["TPE1"] = TPE1(text=talk.speaker)
+    mp3["TPE2"] = TPE2(text=talk.speaker)
+    mp3["TRCK"] = TRCK(text=str(talk.id))
+    mp3["TDRC"] = TDRC(text=str(app.config["GB_FRIDAY"][:-4]))
+    mp3["COMM"] = COMM(text=talk.description)
+    mp3["TCMP"] = TCMP(text="1")
 
-    with open(app.config["IMG_DIR"] + '/alltalksicon.png', 'rb') as albumart:
-        mp3['APIC'] = APIC(
-                          mime='image/png',
-                          type=3, desc='Front cover',
-                          data=albumart.read()
-                        )
+    with open(app.config["IMG_DIR"] + "/alltalksicon.png", "rb") as albumart:
+        mp3["APIC"] = APIC(
+            mime="image/png", type=3, desc="Front cover", data=albumart.read()
+        )
     mp3.save()
-    
+
     # Create files for later CD burning
     # Split the mp3 into 5min (300k ms) slices
     os.makedirs(get_cd_dir_for_talk(talk.id))
-    for idx,cd_file in enumerate(hq_mp3[::300000]):
-         cd_file.export(get_cd_dir_for_talk(talk.id) +
-                        '/' +
-                        str(idx).zfill(2) +
-                        '.wav',
-                        format="wav")
+    for idx, cd_file in enumerate(hq_mp3[::300000]):
+        cd_file.export(
+            get_cd_dir_for_talk(talk.id) + "/" + str(idx).zfill(2) + ".wav",
+            format="wav",
+        )
 
 
 @click.command()
@@ -96,16 +113,34 @@ def convert_talks():
     """Create production files (MP3 and CD) from edited files"""
 
     # Make sure we only run one of these at a time
-    only_once_preventer = singleton.SingleInstance(flavor_id='convert_talks')
+    only_once_preventer = singleton.SingleInstance(flavor_id="convert_talks")
 
-    gb_year = str(app.config['GB_FRIDAY'][2:4])
+    gb_year = str(app.config["GB_FRIDAY"][2:4])
     gb_prefix = "gb" + gb_year + "-"
 
     # Work out which files need to be converted by looking at the filesystem
     # If a talk has an edited file but no converted file, convert it!
 
-    edited_files = set([x.name.replace('_EDITED.mp3','').replace(gb_prefix,'') for x in os.scandir(app.config['EDITED_UPLOAD_DIR']) if x.name.endswith('EDITED.mp3')]) or set()
-    processed_files = set([x.name.replace('mp3.mp3','').replace(gb_prefix,'') for x in os.scandir(app.config['PROCESSED_DIR']) if x.name.endswith('mp3.mp3')]) or set()
+    edited_files = (
+        set(
+            [
+                x.name.replace("_EDITED.mp3", "").replace(gb_prefix, "")
+                for x in os.scandir(app.config["EDITED_UPLOAD_DIR"])
+                if x.name.endswith("EDITED.mp3")
+            ]
+        )
+        or set()
+    )
+    processed_files = (
+        set(
+            [
+                x.name.replace("mp3.mp3", "").replace(gb_prefix, "")
+                for x in os.scandir(app.config["PROCESSED_DIR"])
+                if x.name.endswith("mp3.mp3")
+            ]
+        )
+        or set()
+    )
 
     talks = edited_files
     talks.difference_update(processed_files)
@@ -120,9 +155,22 @@ def convert_talks():
 
 
 def burn_cd(talk_id, cd_index, cd_writer):
-    talk_cd_files = [x for x in list(os.scandir(get_cd_dir_for_talk(talk_id))) if x.is_file()]
+    talk_cd_files = [
+        x for x in list(os.scandir(get_cd_dir_for_talk(talk_id))) if x.is_file()
+    ]
     cd_files = talk_cd_files[::15][cd_index]
-    subprocess.call(['wodim', 'dev=/dev/sg' + cd_writer, '-dao' , '-pad', '-audio', '-eject', cd_files])
+    subprocess.call(
+        [
+            "wodim",
+            "dev=/dev/sg" + cd_writer,
+            "-dao",
+            "-pad",
+            "-audio",
+            "-eject",
+            cd_files,
+        ]
+    )
+
 
 @click.command(name="createdb")
 @with_appcontext
--- gbtalks/models.py
+++ gbtalks/models.py
@@ -1,12 +1,13 @@
 from . import db
 
+
 class Talk(db.Model):
-    __tablename__ = 'talks'
+    __tablename__ = "talks"
 
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String)
     description = db.Column(db.String)
-    speaker =  db.Column(db.String)
+    speaker = db.Column(db.String)
 
     start_time = db.Column(db.DateTime)
     end_time = db.Column(db.DateTime)
@@ -16,15 +17,27 @@ class Talk(db.Model):
     is_priority = db.Column(db.Boolean)
     is_rotaed = db.Column(db.Boolean)
 
-    recorder_name = db.Column(db.String, db.ForeignKey('recorders.name'))
-    editor_name = db.Column(db.String, db.ForeignKey('editors.name'))
+    recorder_name = db.Column(db.String, db.ForeignKey("recorders.name"))
+    editor_name = db.Column(db.String, db.ForeignKey("editors.name"))
 
     def __repr__(self):
-        return "<Talk(id='%d', title='%s', description='%s', start_time='%s', end_time='%s', venue='%s', recorder_name='%s', priority='%s')>" % (self.id, self.title, self.description, self.start_time, self.end_time, self.venue, self.recorder_name, self.is_priority)
+        return (
+            "<Talk(id='%d', title='%s', description='%s', start_time='%s', end_time='%s', venue='%s', recorder_name='%s', priority='%s')>"
+            % (
+                self.id,
+                self.title,
+                self.description,
+                self.start_time,
+                self.end_time,
+                self.venue,
+                self.recorder_name,
+                self.is_priority,
+            )
+        )
 
 
 class Recorder(db.Model):
-    __tablename__ = 'recorders'
+    __tablename__ = "recorders"
 
     name = db.Column(db.String, primary_key=True)
     max_shifts_per_day = db.Column(db.Integer)
@@ -34,15 +47,17 @@ class Recorder(db.Model):
 
 
 class Editor(db.Model):
-    __tablename__ = 'editors'
+    __tablename__ = "editors"
 
     name = db.Column(db.String, primary_key=True)
     talks = db.relationship("Talk", backref="edited_by", order_by="Talk.start_time")
+
 
 ### Models for Google login
 
 from flask_login import LoginManager, UserMixin
 from flask_dance.consumer.storage.sqla import OAuthConsumerMixin
+
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
